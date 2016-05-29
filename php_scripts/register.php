6e90a8730c7adfde04fd91ca3e019475eeb1a902
miskoajkula <cofilicavujovic@gmail.com>
2016-05-29 14:14:20 +0200

    Create register.php

---
+++ php_scripts/register.php
@@ -0,0 +1,19 @@
+<?php 
+
+	if (isset($_POST["Token"])) {
+		
+		   $_uv_Token=$_POST["Token"];
+
+		   $conn = mysqli_connect("localhost","root","","gcm") or die("Error connecting");
+
+		   $q="INSERT INTO gcmregusers (Token) VALUES ( '$_uv_Token') "
+              ." ON DUPLICATE KEY UPDATE Token = '$_uv_Token';";
+              
+      mysqli_query($conn,$q) or die(mysqli_error($conn));
+
+      mysqli_close($conn);
+
+	}
+
+
+ ?>
