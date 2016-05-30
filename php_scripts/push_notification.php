17004e0d56340ecb9b732464dcaedeacd1038631
Filip Vujovic <cofilicavujovic@gmail.com>
2016-05-30 22:03:50 +0200

    Create push_notification.php

---
+++ php_scripts/push_notification.php
@@ -0,0 +1,55 @@
+<?php 
+
+	function send_notification ($tokens, $message)
+	{
+		$url = 'https://fcm.googleapis.com/fcm/send';
+		$fields = array(
+			 'registration_ids' => $tokens,
+			 'data' => $message
+			);
+
+		$headers = array(
+			'Authorization:key = YOUR_KEY ',
+			'Content-Type: application/json'
+			);
+
+	   $ch = curl_init();
+       curl_setopt($ch, CURLOPT_URL, $url);
+       curl_setopt($ch, CURLOPT_POST, true);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+       $result = curl_exec($ch);           
+       if ($result === FALSE) {
+           die('Curl failed: ' . curl_error($ch));
+       }
+       curl_close($ch);
+       return $result;
+	}
+	
+
+	$conn = mysqli_connect("localhost","root","","fcm");
+
+	$sql = " Select Token From users";
+
+	$result = mysqli_query($conn,$sql);
+	$tokens = array();
+
+	if(mysqli_num_rows($result) > 0 ){
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			$tokens[] = $row["Token"];
+		}
+	}
+
+	mysqli_close($conn);
+
+	$message = array("message" => " FCM PUSH NOTIFICATION TEST MESSAGE");
+	$message_status = send_notification($tokens, $message);
+	echo $message_status;
+
+
+
+ ?>
